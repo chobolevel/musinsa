b803fae741f2e046bf09979bdcf6ea4effbbe75c
chobolevel <rodaka123@naver.com>
2025-08-30 15:44:02 +0900

    feat: add audit configuration

---
+++ src/main/kotlin/com/musinsa/common/config/AuditConfiguration.kt
@@ -0,0 +1,18 @@
+package com.musinsa.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.auditing.DateTimeProvider
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.time.OffsetDateTime
+import java.util.*
+
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+class AuditConfiguration {
+
+    @Bean
+    fun auditingDateTimeProvider(): DateTimeProvider {
+        return DateTimeProvider { Optional.of(OffsetDateTime.now()) }
+    }
+}
