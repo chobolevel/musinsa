6ced2660f9614b9530e4f826bc08670d59637151
chobolevel <rodaka123@naver.com>
2025-09-04 18:38:55 +0900

    feat: add user update & resign api

--- src/main/kotlin/com/musinsa/user/controllerV1/UserControllerV1.kt
+++ src/main/kotlin/com/musinsa/user/controllerV1/UserControllerV1.kt
@@ -3,13 +3,16 @@ package com.musinsa.user.controllerV1
 import com.musinsa.common.dto.CommonResponse
 import com.musinsa.common.extension.getUserId
 import com.musinsa.user.dto.CreateUserRequest
+import com.musinsa.user.dto.UpdateUserRequest
 import com.musinsa.user.dto.UserResponse
 import com.musinsa.user.serivce.UserService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -33,6 +36,22 @@ class UserControllerV1(
     @GetMapping("/user/me")
     fun getMe(principal: Principal): ResponseEntity<CommonResponse> {
         val result: UserResponse = service.getUser(principal.getUserId())
+        return ResponseEntity.ok().body(CommonResponse(data = result))
+    }
+
+    @PutMapping("/user/me")
+    fun updateMe(
+        principal: Principal,
+        @Valid @RequestBody
+        request: UpdateUserRequest
+    ): ResponseEntity<CommonResponse> {
+        val result: Long = service.updateUser(userId = principal.getUserId(), request = request)
+        return ResponseEntity.ok().body(CommonResponse(data = result))
+    }
+
+    @DeleteMapping("/user/resign")
+    fun resign(principal: Principal): ResponseEntity<CommonResponse> {
+        val result: Boolean = service.resignUser(userId = principal.getUserId())
         return ResponseEntity.ok().body(CommonResponse(data = result))
     }
 }
