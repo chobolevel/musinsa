4cfa291c5634c6cd3cab1f17e98af62ee94fc459
chobolevel <rodaka123@naver.com>
2025-09-17 15:54:32 +0900

    refactor: remove annotation
    already added bean container by auto configuration

--- src/main/kotlin/com/musinsa/user/entity/UserRepository.kt
+++ src/main/kotlin/com/musinsa/user/entity/UserRepository.kt
@@ -1,9 +1,7 @@
 package com.musinsa.user.entity
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface UserRepository : JpaRepository<User, Long> {
 
     fun findByIdAndIsDeletedFalse(id: Long): User?
