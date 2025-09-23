ff112f8f9461bbe6cf2097d7f7734559c5b067d7
chobolevel <rodaka123@naver.com>
2025-09-23 22:31:47 +0900

    refactor: properties bean injection way
    file -> package

--- src/main/kotlin/com/musinsa/MusinsaApplication.kt
+++ src/main/kotlin/com/musinsa/MusinsaApplication.kt
@@ -1,12 +1,11 @@
 package com.musinsa
 
-import com.musinsa.common.properties.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties::class)
+@ConfigurationPropertiesScan("com.musinsa.common.properties")
 class MusinsaApplication
 
 fun main(args: Array<String>) {
