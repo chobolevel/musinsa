e637ae83cb306fe57fcb61d6c708e76de1034724
chobolevel <rodaka123@naver.com>
2025-09-25 22:21:26 +0900

    refactor: apply SRP(Single Responsibility Principal)

--- src/main/kotlin/com/musinsa/auth/util/TokenProvider.kt
+++ src/main/kotlin/com/musinsa/auth/util/TokenProvider.kt
@@ -1,18 +1,14 @@
 package com.musinsa.auth.util
 
-import com.musinsa.auth.dto.JwtResponse
 import com.musinsa.common.exception.ErrorCode
 import com.musinsa.common.exception.UnauthorizedException
 import com.musinsa.common.properties.JwtProperties
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Header
 import io.jsonwebtoken.JwtException
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
-import org.slf4j.LoggerFactory
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import java.util.Date
 import java.util.concurrent.TimeUnit
@@ -20,66 +16,42 @@ import java.util.concurrent.TimeUnit
 @Component
 class TokenProvider(
     private val jwtProperties: JwtProperties,
-    private val userDetailService: UserDetailsService
 ) {
 
-    private val logger = LoggerFactory.getLogger(this.javaClass)
+    private final val now = Date()
 
-    fun generateToken(authentication: Authentication): JwtResponse {
-        val now = Date()
-        val accessTokenExpiredAt = Date(now.time + TimeUnit.HOURS.toMillis(1))
-        val refreshTokenExpiredAt = Date(now.time + TimeUnit.DAYS.toMillis(30))
-        val accessToken = issueAccessToken(
-            issuedAt = now,
-            expiration = accessTokenExpiredAt,
-            authentication = authentication
-        )
-        val refreshToken = issueRefreshToken(
-            issuedAt = now,
-            expiration = refreshTokenExpiredAt,
-            authentication = authentication
-        )
-        return JwtResponse(
-            accessToken = accessToken,
-            refreshToken = refreshToken,
-        )
-    }
-
-    private fun issueAccessToken(issuedAt: Date, expiration: Date, authentication: Authentication): String {
+    // TODO Authentication으로 왔다 갔다 하지 않는 방법으로 해보기(단일 책임 원칙 구현)
+    // 단일 책임 원칙에 따르면 이 로직이 수정되기 위해서는 반드시 하나의 사유에 의해서만 가능하도록 설계되어야 함
+    fun generateAccessToken(id: Long): String {
+        val expiredAt = Date(now.time + TimeUnit.MINUTES.toMillis(30))
         return Jwts.builder()
             .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
             .setIssuer(jwtProperties.issuer)
-            .setIssuedAt(issuedAt)
-            .setExpiration(expiration)
-            .setSubject(authentication.name)
+            .setIssuedAt(now)
+            .setExpiration(expiredAt)
+            .setSubject(id.toString())
             .signWith(SignatureAlgorithm.HS256, jwtProperties.secret)
             .compact()
     }
 
-    private fun issueRefreshToken(issuedAt: Date, expiration: Date, authentication: Authentication): String {
+    fun generateRefreshToken(id: Long): String {
+        val expiredAt = Date(now.time + TimeUnit.MINUTES.toMillis(30))
         return Jwts.builder()
             .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
             .setIssuer(jwtProperties.issuer)
-            .setIssuedAt(issuedAt)
-            .setExpiration(expiration)
-            .setSubject(authentication.name)
+            .setIssuedAt(now)
+            .setExpiration(expiredAt)
+            .setSubject(id.toString())
             .signWith(SignatureAlgorithm.HS256, jwtProperties.secret)
             .compact()
     }
 
-    fun getAuthentication(token: String): Authentication? {
-        return try {
-            validateToken(token)
-            val claims = Jwts.parser()
-                .setSigningKey(jwtProperties.secret)
-                .parseClaimsJws(token)
-                .body
-            val userDetails = userDetailService.loadUserByUsername(claims.subject)
-            UsernamePasswordAuthenticationToken(userDetails, token, userDetails.authorities)
-        } catch (e: Exception) {
-            logger.warn("Token is invalid", e)
-            null
-        }
+    fun getId(token: String): Long {
+        val claims: Claims = Jwts.parser()
+            .setSigningKey(jwtProperties.secret)
+            .parseClaimsJws(token)
+            .body
+        return claims.subject.toLong()
     }
 
     fun validateToken(token: String): Boolean {
