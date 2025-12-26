c6bc57f6faee4dbdafe07d923e23722c802ab6ad
chobolevel <rodaka123@naver.com>
2025-12-26 17:19:09 +0900

    refactor: add admin path

--- src/main/kotlin/com/musinsa/product/controller/v1/AdminProductController.kt
+++ src/main/kotlin/com/musinsa/product/controller/v1/AdminProductController.kt
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/admin")
 class AdminProductController(
     private val service: ProductService,
     private val validator: ProductParameterValidator
