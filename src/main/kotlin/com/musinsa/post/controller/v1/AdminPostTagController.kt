1df8635eeda80899b4acbf236515f52dedffbf0e
chobolevel <rodaka123@naver.com>
2026-02-04 17:07:01 +0900

    fix: remove useless field

--- src/main/kotlin/com/musinsa/post/controller/v1/AdminPostTagController.kt
+++ src/main/kotlin/com/musinsa/post/controller/v1/AdminPostTagController.kt
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/admin")
 class AdminPostTagController(
     private val postTagService: PostTagService,
-    tagService: PostTagService
 ) {
 
     @PostMapping("/post-tags")
