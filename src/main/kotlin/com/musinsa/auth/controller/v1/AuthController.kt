86663d3d1e1fbf45185720e53a8cb97862cd4469
chobolevel <rodaka123@naver.com>
2026-02-26 17:43:06 +0900

    feat: add swagger annotation

--- src/main/kotlin/com/musinsa/auth/controller/v1/AuthController.kt
+++ src/main/kotlin/com/musinsa/auth/controller/v1/AuthController.kt
@@ -5,26 +5,21 @@ import com.musinsa.auth.dto.LoginRequest
 import com.musinsa.auth.dto.ReissueResponse
 import com.musinsa.auth.service.AuthService
 import com.musinsa.common.dto.CommonResponse
-import com.musinsa.common.dto.ErrorResponse
 import com.musinsa.common.exception.ErrorCode
 import com.musinsa.common.exception.InvalidParameterException
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "인증 API", description = "인증을 위한 API를 제공합니다.")
 @RestController
 @RequestMapping("/api/v1")
-@Tag(name = "인증 API", description = "인증을 위한 API를 제공합니다.")
 class AuthController(
     private val service: AuthService
 ) {
@@ -33,28 +28,6 @@ class AuthController(
     @Operation(
         summary = "로그인",
         description = "회원 로그인을 통해 access token/refresh token 발급",
-        responses = [
-            ApiResponse(
-                responseCode = "200",
-                description = "로그인 성공",
-                content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        schema = Schema(implementation = CommonResponse::class)
-                    )
-                ]
-            ),
-            ApiResponse(
-                responseCode = "400",
-                description = "잘못된 요청",
-                content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        schema = Schema(implementation = ErrorResponse::class)
-                    )
-                ]
-            )
-        ]
     )
     fun login(
         @Valid @RequestBody
@@ -68,28 +41,6 @@ class AuthController(
     @Operation(
         summary = "토큰 갱신",
         description = "refresh token 통해 access token 갱신",
-        responses = [
-            ApiResponse(
-                responseCode = "200",
-                description = "토큰 발급 성공",
-                content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        schema = Schema(implementation = CommonResponse::class)
-                    )
-                ]
-            ),
-            ApiResponse(
-                responseCode = "400",
-                description = "잘못된 요청",
-                content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        schema = Schema(implementation = ErrorResponse::class)
-                    )
-                ]
-            )
-        ]
     )
     fun reissue(request: HttpServletRequest): ResponseEntity<CommonResponse> {
         val refreshToken: String = request.getHeader("X-Refresh-Token") ?: throw InvalidParameterException(
@@ -104,28 +55,6 @@ class AuthController(
     @Operation(
         summary = "로그아웃",
         description = "로그아웃을 합니다.",
-        responses = [
-            ApiResponse(
-                responseCode = "200",
-                description = "로그아웃 성공",
-                content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        schema = Schema(implementation = CommonResponse::class)
-                    )
-                ]
-            ),
-            ApiResponse(
-                responseCode = "400",
-                description = "잘못된 요청",
-                content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        schema = Schema(implementation = ErrorResponse::class)
-                    )
-                ]
-            )
-        ]
     )
     fun logout(request: HttpServletRequest): ResponseEntity<CommonResponse> {
         val refreshToken: String = request.getHeader("X-Refresh-Token") ?: throw InvalidParameterException(
